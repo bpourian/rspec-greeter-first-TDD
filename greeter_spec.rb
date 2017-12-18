992d625c491ee11a14d1c2131aec978fa5c4c8ba
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-18 19:42:17 +0000

    updated rspec code to test a different name

--- greeter_spec.rb
+++ greeter_spec.rb
@@ -1,7 +1,17 @@
 require "./greeter"
 
 describe 'Greeter' do
-  it 'greets Rico' do
-    expect(greet('Rico')).to eq 'Hello, Rico, how are you today?'
+  it 'greets Chloe' do
+    expect(greet('Chloe')).to eq 'Hello, Chloe, how are you today?'
   end
 end
+
+
+
+=begin
+it 'greets Rico' do
+  name = 'Rico' # Given
+  result = greet(name) # When
+  expect(result).to eq 'Hello, Rico, how are you today?' # Then
+end
+=end
